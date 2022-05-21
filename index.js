1f8e1c0756907d6ef140fb923e2b3076e749662d
Ionuț Maxim <me+git.github@ionutmaxim.ro>
2022-05-21 20:05:10 +0300

    new theme

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { promises as fs } from 'fs'
 // import * as themeKwan from 'jsonresume-theme-kwan'
-import * as themeKendall from 'jsonresume-theme-kendall'
+// import * as themeKendall from 'jsonresume-theme-kendall'
+import * as themeStackOverflow from 'jsonresume-theme-stackoverflow'
+
 import puppeteer from 'puppeteer'
 import { render } from 'resumed'
 
 const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
-const html = await render(resume, themeKendall)
+const html = await render(resume, themeStackOverflow)
 
 const browser = await puppeteer.launch()
 const page = await browser.newPage()
